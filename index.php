7be27dec45fc3bdb2e138c558d2d0103c2a36644
yusup <irada@qq.com>
2013-10-12 00:53:33 +0800

    index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php include 'classes/file.helper.php';?>
+<html>
+	<head>
+		<title>ijadkarsoft</title>
+	</head>
+	<body>
+		<?php 
+			$file = new file();
+			$file->to_utf("music");
+		?>
+	</body>
+</html>
